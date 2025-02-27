3ab1e4a3a15c1f8aeb0a5bedc3699ba7123da372
lena-dan82 <e54457@yandex.ru>
2025-02-27 22:08:07 +0300

    add cycle code

--- main.py
+++ main.py
@@ -23,6 +23,16 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-        pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if balloon_x < mouse_x < balloon_x + balloon_width and balloon_y < mouse_y < balloon_y + balloon_height:
+                balloon_x = random.randint(0, SCREEN_WIDTH - balloon_width)
+                balloon_y = random.randint(0, SCREEN_HEIGHT - balloon_height)
+    screen.blit(balloon_img, (balloon_x, balloon_y))
+    pygame.display.update()
 
 pygame.quit()
